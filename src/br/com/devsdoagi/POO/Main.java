22dc4b094d7a54ef45e77a81f03d5292fa99158e
BrunoRS29 <brunorosantos29@gmail.com>
2025-08-15 11:49:13 -0300

    aula 15/08

--- src/br/com/devsdoagi/POO/Main.java
+++ src/br/com/devsdoagi/POO/Main.java
@@ -1,12 +1,19 @@
 package br.com.devsdoagi.POO;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Aluno a1 = new Aluno();
 
-        a1.nome = "Bruno";
-        a1.nota1 = 2;
-        a1.nota2 = 6;
+
+        System.out.println("Digite o nome:");
+        a1.nome = sc.next();
+        System.out.println("Nota 1:");
+        a1.nota1 = sc.nextDouble();
+        System.out.println("Nota 2:");
+        a1.nota2 = sc.nextDouble();
 
         a1.exibir();
     }
